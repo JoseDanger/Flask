fb10ffbc89f69e6d4771dc1597a3f48ea5364f95
JohnnyDanger <joshua.cramond@gmail.com>
2021-03-23 16:50:26 +1300

    newer

--- app.py
+++ app.py
@@ -2,15 +2,16 @@ from flask import Flask, jsonify, request
 import PAYE
 from flask_cors import CORS
 
-
 app = Flask(__name__)
 CORS(app)
+
 
 @app.route('/')
 def hello_world():
     return 'Hello World!'
 
-@app.route('/getNetIncome/' , method=['POST'])
+
+@app.route('/getNetIncome/', method=['POST'])
 def netIncome():
     gross_income = request.get_json()
     print(gross_income)
@@ -18,7 +19,5 @@ def netIncome():
     return jsonify(gross - PAYE.totalTax(gross))
 
 
-
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
-
