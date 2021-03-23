5c51c160d0b5e457eb6593ffecf616f660df664a
JohnnyDanger <joshua.cramond@gmail.com>
2021-03-23 16:49:05 +1300

    new

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, jsonify, request
 import PAYE
 from flask_cors import CORS
 
@@ -10,8 +10,15 @@ CORS(app)
 def hello_world():
     return 'Hello World!'
 
+@app.route('/getNetIncome/' , method=['POST'])
+def netIncome():
+    gross_income = request.get_json()
+    print(gross_income)
+    gross = float(gross_income["gross_income"])
+    return jsonify(gross - PAYE.totalTax(gross))
+
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host='0.0.0.0')
 
